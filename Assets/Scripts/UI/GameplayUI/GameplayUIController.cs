36f6c7534032bd2ac874cc9e288fc47aca51a1e4
sohamPatkar <spatkar398@gmail.com>
2025-05-26 21:44:15 +0530

    Added Undo function invokation

--- Assets/Scripts/UI/GameplayUI/GameplayUIController.cs
+++ Assets/Scripts/UI/GameplayUI/GameplayUIController.cs
@@ -16,6 +16,8 @@ namespace Command.UI
 
         public void Show() => gameplayView.EnableView();
 
+        public void OnUndoButtonClicked() => GameService.Instance.CommandInvoker.Undo();
+
         public void SetTurnNumber(int turnNumber) => gameplayView.SetTurnText($"Turn: {turnNumber}");
 
         public void ShowActionOverlay(int activePlayer)
--- Assets/Scripts/UI/GameplayUI/GameplayUIView.cs
+++ Assets/Scripts/UI/GameplayUI/GameplayUIView.cs
@@ -17,10 +17,12 @@ namespace Command.UI
         [SerializeField] private Color EnemyOverlayColor;
         [SerializeField] private Color ActionSelectionOverlayColor;
         [SerializeField] private Image backgroundImage;
+        [SerializeField] private Button undoButton;
 
-        public void SetController(GameplayUIController controllerToSet) 
+        public void SetController(GameplayUIController controllerToSet)
         {
             controller = controllerToSet;
+            undoButton.onClick.AddListener(controller.OnUndoButtonClicked);
             missedText.canvasRenderer.SetAlpha(0);
         }
 
@@ -32,7 +34,7 @@ namespace Command.UI
 
         public void ShowPlayerOverlay(int targetPlayer, OverlayColorType overlayColorType)
         {
-            switch(targetPlayer)
+            switch (targetPlayer)
             {
                 case 1:
                     Player1BackgroundOverlay.enabled = true;
@@ -62,7 +64,7 @@ namespace Command.UI
 
         public void SetOverlayColor(Image overlayImage, OverlayColorType colorType)
         {
-            switch(colorType)
+            switch (colorType)
             {
                 case OverlayColorType.Friendly:
                     overlayImage.color = FriendlyOverlayColor;
@@ -82,7 +84,7 @@ namespace Command.UI
         {
             backgroundImage.gameObject.SetActive(true);
             backgroundImage.sprite = bgSprite;
-        } 
+        }
     }
 
     [Serializable]
