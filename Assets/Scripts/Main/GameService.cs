86cb71352425cb8de07107c4a7ca74c0082adeb1
sohamPatkar <spatkar398@gmail.com>
2025-05-27 14:09:10 +0530

    Added Replay

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -10,6 +10,7 @@ using Command.Battle;
 using Command.Actions;
 using Command.Commands.AbstractCommands;
 using Command.Commands;
+using Command.Replay;
 
 namespace Command.Main
 {
@@ -30,6 +31,7 @@ namespace Command.Main
         public BattleService BattleService { get; private set; }
         public PlayerService PlayerService { get; private set; }
         public CommandInvoker CommandInvoker { get; private set; }
+        public ReplayService ReplayService { get; private set; }
 
         [SerializeField] private UIService uiService;
         public UIService UIService => uiService;
@@ -51,6 +53,7 @@ namespace Command.Main
             InputService = new InputService();
             BattleService = new BattleService(battleScriptableObjects);
             PlayerService = new PlayerService();
+            ReplayService = new ReplayService();
             uiService.Init(battleScriptableObjects.Count);
         }
 
